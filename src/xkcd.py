948ac7ae5a44faf4e408b15df56de9c71151e3f5
zeviel <77536370+zeviel@users.noreply.github.com>
2022-08-25 19:31:37 +0800

    Create xkcd.py

---
+++ src/xkcd.py
@@ -0,0 +1,18 @@
+from requests import get
+
+class Xkcd:
+	def __init__(self):
+		self.api = "https://xkcd.com"
+		self.headers = {
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+		}
+	
+	def get_current_comics(self):
+		return get(
+			f"{self.api}/info.0.json",
+			headers=self.headers).json()
+
+	def get_comics_info(self, comics_id: int):
+		return get(
+			f"{self.api}/{comics_id}/info.0.json",
+			headers=self.headers).json()
